9611a4ad47599989813072223c26e18dff21b654
Hao <sunskyhao@outlook.com>
2022-09-24 00:46:19 -0400

    aesthetics

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+#[allow(non_snake_case)]
+
 use std::io;
+use std::process::Command;
+
+const HANGINGMAN: &'static [&'static str] = &[
+    "+-----+\n|     |\n|     0\n|    /|\\ \n|    / \\ \n|\n===+=======",
+    "+-----+\n|     |\n|     0\n|    /|\\ \n|    /\n|\n===+=======",
+    "+-----+\n|     |\n|     0\n|    /|\\ \n|\n|\n===+=======",
+    "+-----+\n|     |\n|     0\n|    /|\n|\n|\n===+=======",
+    "+-----+\n|     |\n|     0\n|     |\n|\n|\n===+=======",
+    "+-----+\n|     |\n|     0\n|\n|\n|\n===+=======",
+    "+-----+\n|     |\n|\n|\n|\n|\n===+=======",
+    "+-----+\n|\n|\n|\n|\n|\n===+=======",
+    "\n|\n|\n|\n|\n|\n===+=======",
+    "\n\n\n\n\n\n===+=======",
+    "\n\n\n\n\n\n"];
 
 struct Hangman{
     answer: Vec<char>,
     progress: Vec<bool>,
-    life: isize,
+    life: usize,
 }
 
 impl Hangman{
@@ -13,22 +29,31 @@ impl Hangman{
         Self {
             answer: char_list,
             progress: no_progress,
-            life: 6,
+            life: 10,
         }
     }
 
     fn guess(&mut self, guess: char) {
-        self.life -= 1;
+        let mut isAccurateGuess = false;
 
         for i in 0..self.answer.len(){
             if guess == self.answer[i]{
                 self.progress[i] = true;
+                isAccurateGuess = true
             }
+        }
+
+        if !isAccurateGuess{
+            if self.life > 0{
+                self.life -= 1;
+            }else{
+                println!("GAME OVER")
+            }    
         }
     }
 
     fn display(&self) {
-        println!("{} tries left", self.life);
+        println!("{}", HANGINGMAN[self.life]);
 
         for i in 0..self.answer.len(){
             if self.progress[i] == true {
@@ -37,6 +62,7 @@ impl Hangman{
                 print!("{} ", '_');
             }
         }
+    println!("")
     }
 }
 
@@ -45,12 +71,19 @@ fn main() {
     let mut game = Hangman::new(String::from("malloc"));
 
     while !game.progress.iter().all(|x| *x){
-        println!("");
         let mut user_input = String::new();
         stdin.read_line(&mut user_input).unwrap();
+        clear();
 
         game.guess(user_input.chars().nth(0).unwrap());
         game.display();
     }
+}
 
+fn clear(){
+    if cfg!(target_os = "windows") {
+        Command::new("cls").status().unwrap();
+    }else{
+        Command::new("clear").status().unwrap();
+    }
 }
