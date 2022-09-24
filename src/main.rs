bfd8b4bc2c54eb818dde9ddd65856349ab28f429
Hao <sunskyhao@outlook.com>
2022-09-23 23:50:13 -0400

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
 
 struct Hangman{
-    answer: Vec<u8>,
+    answer: Vec<char>,
     progress: Vec<bool>,
     life: isize,
 }
 
 impl Hangman{
     fn new(answer: String) -> Self {
-        let char_list = answer.into_bytes();
+        let char_list: Vec<char> = answer.chars().collect();
         let no_progress = vec![false; char_list.len()];
         Self {
             answer: char_list,
@@ -20,7 +20,6 @@ impl Hangman{
     fn guess(&mut self, guess: char) {
         self.life -= 1;
 
-        let guess = guess as u8;
         for i in 0..self.answer.len(){
             if guess == self.answer[i]{
                 self.progress[i] = true;
@@ -30,29 +29,26 @@ impl Hangman{
 
     fn display(&self) {
         println!("{} tries left", self.life);
-        let mut toprint: char;
 
         for i in 0..self.answer.len(){
             if self.progress[i] == true {
-                toprint = self.answer[i] as char;
+                print!("{} ", self.answer[i]);
             }else{
-                toprint = '_';
+                print!("{} ", '_');
             }
-
-            print!("{} ", toprint);
         }
     }
 }
 
 fn main() {
     let stdin = io::stdin();
-    let mut user_input = String::new();
     let mut game = Hangman::new(String::from("malloc"));
-    //let flag = false;
 
     while !game.progress.iter().all(|x| *x){
-        println!("line runs");
+        println!("");
+        let mut user_input = String::new();
         stdin.read_line(&mut user_input).unwrap();
+
         game.guess(user_input.chars().nth(0).unwrap());
         game.display();
     }
